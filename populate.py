6e945b80a37ae46b9b7c67b49ca08e4e24ed9ba0
Francisco Torre <esfrantorre@hotmail.com>
2017-08-03 18:14:41 -0300

    Las consultas de influx se hacen en ingles. Permite hacer consultas con order by, limit y offset

--- populate.py
+++ populate.py
@@ -1,6 +1,7 @@
 import wenuclient
 import random
 import string
+from wenuapi.models.role import Role
 
 server = wenuclient.Client('http://localhost:8080')
 
@@ -28,14 +29,13 @@ for level in levels:
     levelobj = server.Level(**level)
     levelobj.create()
 
-
-server.Role(rolename='admin').create()
+roleResponse = server.Role(rolename = 'admin').create()
 server.Role(rolename='user').create()
+userResponse = server.User(username='admin', password= "1234").create()
+
+server.Roletable(user_id = userResponse.get('_id'),role_id = roleResponse.get('_id')).create()
 
 server.Action(mote_id=1, command='turn_off', arguments='').create()
 server.Action(mote_id=2, command='turn_off', arguments='').create()
 server.Action(mote_id=3, command='turn_off', arguments='').create()
 server.Action(mote_id=4, command='turn_off', arguments='').create()
-
-server.User(username='admin', password= 1234).create()
-server.Roletable(user_id=1,role_id=1).create()
--- wenuapi/influx_db_handler.py
+++ wenuapi/influx_db_handler.py
@@ -6,6 +6,15 @@ from collections import OrderedDict
 import string
 import re
 
+translator = {
+            'mote_id': 'mota_id',
+            'current': 'corriente',
+            'movement': 'movimiento',
+            'temperature': 'temperatura',
+            'voltage': 'voltaje',
+            'time': 'time',
+        }
+
 def make_query(query,client):
     result = client.query(query);
     #print result
@@ -33,6 +42,8 @@ def reParser(string):
             value = string.partition(operator)
             string = value[2]
             array[0] = operator
+    if not string.replace('.','',1).isdigit():
+        string = "'" + string +"'"
     array.append(string)
     return array
 
@@ -40,19 +51,42 @@ def flask_to_influxdb_query(args):
     # FIXME: only supports simple where statements
     query_args = []
     where = args.get('where')
+    limit = None
+    order = None
+    offset = None
     if where is not None:
         where_args = json.loads(where, object_pairs_hook=OrderedDict)
-        print where_args
+        if 'ORDER_DESC' in where_args:
+            order = where_args['ORDER_DESC']
+            order_type = "DESC"
+            del where_args['ORDER_DESC']
+        if 'ORDER_ASC' in where_args:
+            order = where_args['ORDER_ASC']
+            order_type = "ASC"
+            del where_args['ORDER_ASC']
+        if 'LIMIT' in where_args:
+            limit = where_args['LIMIT']
+            del where_args['LIMIT']
+        if 'OFFSET' in where_args:
+            if limit:
+                offset = where_args['OFFSET']
+                print 'Hay limit y offset'
+            del where_args['OFFSET']
         where_condition = []
         for key, val in where_args.items():
-            valDic = reParser(val)
-            print valDic
-            where_condition.append(" {} {} {}".format(key,valDic[0],valDic[1]))
+            if key in translator:
+                valDic = reParser(val)
+                where_condition.append(" {} {} {}".format(translator[key],valDic[0],valDic[1]))
         if where_condition:
             query_args.extend(('WHERE', ' AND '.join(where_condition)))
-
-    return ' '.join(query_args)
-
+            query = ' '.join(query_args)
+            if order:
+                query = "{} ORDER BY {} {}".format(query, order,order_type)
+            if limit:
+                query = "{} LIMIT {}".format(query,limit)
+                if offset:
+                    query = "{} OFFSET {}".format(query,offset)
+    return query
 
 class InfluxDBHandler(object):
     def __init__(self, host, port, username, password, db, app):
@@ -60,29 +94,14 @@ class InfluxDBHandler(object):
         self.username = username
         self.password = password
         self.influx = InfluxDBClient(host, port, username, password, db)
-        app.route('/measurement/<mota_id>', methods=['GET'])(self.query)
         app.route('/measurement', methods=['GET'])(self.list)
 
     @requires_auth
     def list(self):
         query_filter = flask_to_influxdb_query(request.args)
-        print query_filter
-        query = "SELECT * FROM medicion {} ORDER BY time DESC LIMIT 50".format(query_filter)
-        #query = "SELECT * FROM medicion WHERE mota_id = 'linti_cocina' ORDER BY time DESC LIMIT 50"
+        #print query_filter
+        query = "SELECT * FROM medicion {} ".format(query_filter)
+        #query = "SELECT * FROM medicion WHERE mota_id = 'linti_cocina' ORDER BY mota_id DESC LIMIT 50"
         print query
         measurements = make_query(query,self.influx)
         return json.dumps({'_items': measurements})
-
-    @requires_auth
-    def query(self,mota_id):
-        query_filter = flask_to_influxdb_query(request.args)
-        if not query_filter:
-            query_filter = "WHERE mota_id ='%s'" % mota_id
-        else:
-            query_name = " AND mota_id ='%s'" % mota_id
-            query_filter = query_filter + query_name
-        query = "SELECT * FROM medicion {} ORDER BY time DESC LIMIT 50".format(query_filter)
-        measurements = make_query(query,self.influx)
-        return json.dumps({'_items': measurements})
-
-
