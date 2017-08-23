5d15c5e185675c46e757058ddcd94991620d1634
Fernando LÃpez <flopez@linti.unlp.edu.ar>
2017-08-23 18:11:26 -0300

    Task runner con tarea para enviar datos a influxdb

--- wenuapi/settings.py
+++ wenuapi/settings.py
@@ -1,3 +1,4 @@
+import urllib
 from eve_sqlalchemy.decorators import registerSchema
 from .models.action import Action
 from .models.user import User
@@ -9,7 +10,17 @@ from .models.measurement import Measurement
 from .models.mote import Mote
 from secrets import database_username, database_password
 from secrets import influxdb_username, influxdb_password
+from secrets import rabbitmq_username, rabbitmq_password
 
+rabbitmq_hostname = 'mosquitto'
+rabbitmq_vhost = 'wenuapi'
+
+rabbitmq_url = 'amqp://{}:{}@{}:5672/{}'.format(
+    rabbitmq_username,
+    urllib.quote(rabbitmq_password),
+    rabbitmq_hostname,
+    rabbitmq_vhost,
+)
 
 use_influxdb = True
 influxdb_host = 'influxdb.linti.unlp.edu.ar'
