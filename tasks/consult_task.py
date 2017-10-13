ef760d8ffaef5e7a4a0c10e04c8eace8be8bbb9e
Sofía Martin <smartin@linti.unlp.edu.ar>
2017-10-13 16:54:00 -0300

    task mod estado del viewed

--- tasks/consult_task.py
+++ tasks/consult_task.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from common_cfg import *
 from .consult_cfg import *
+import wenuclient 
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as mqttpublish
 import struct 
@@ -8,11 +9,20 @@ import struct
 
 def run():
     valores = struct.pack("BBB",1,1,1)
-    
-    
-    #~ client.connect(mqtt_server, mqtt_port)
-    print(valores)
-    mqttpublish.single("mota1", payload =valores,hostname=mqtt_server, port=mqtt_port)
+    server = "http://163.10.10.118/wenuapi"
+    user = "admin"
+    password = ***REMOVED***
+    session = wenuclient.get_session(
+                            '/'.join((server, 'login')),
+                            user,
+                            password,
+                        )
+    client = wenuclient.Client(server, session)
+    lista = client.Action.where(viewed = False)
+    for each in lista:
+        mqttpublish.single("motaID/accion", payload =valores,hostname=mqtt_server, port=mqtt_port)
+        each.viewed = True
+        each.save()
     
 if __name__ == '__main__':
     run()
