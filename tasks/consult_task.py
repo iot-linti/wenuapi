9d33354ade3e87661bbb0724fd1c437d6f8f3eb1
Sofía Martin <smartin@linti.unlp.edu.ar>
2017-10-13 17:22:15 -0300

    mod task apagar aire

--- tasks/consult_task.py
+++ tasks/consult_task.py
@@ -5,12 +5,13 @@ import wenuclient
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as mqttpublish
 import struct 
+import time
 #https://pypi.python.org/pypi/paho-mqtt/1.1#id17
 
 def run():
     valores = struct.pack("BBB",1,1,1)
     server = "http://163.10.10.118/wenuapi"
-    user = "admin"
+    user = ***REMOVED***
     password = ***REMOVED***
     session = wenuclient.get_session(
                             '/'.join((server, 'login')),
@@ -20,7 +21,10 @@ def run():
     client = wenuclient.Client(server, session)
     lista = client.Action.where(viewed = False)
     for each in lista:
-        mqttpublish.single("motaID/accion", payload =valores,hostname=mqtt_server, port=mqtt_port)
+        print("hay entrada")
+        for cada in range(3):
+            mqttpublish.single("motaID/accion", payload =valores,hostname=mqtt_server, port=mqtt_port)
+            time.sleep(0.2)
         each.viewed = True
         each.save()
     
